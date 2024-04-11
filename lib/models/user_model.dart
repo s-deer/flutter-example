95e429f70c15a0ce2da4ee29565665c69a5d33aa
arl <arl@anadeainc.com>
2024-04-11 08:37:06 +0300

    add user model

---
+++ lib/models/user_model.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class UserModel extends Equatable {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String userName;
+  final String email;
+
+  const UserModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.userName,
+    required this.email,
+  });
+
+  @override
+  List<Object> get props {
+    return [
+      id,
+      firstName,
+      lastName,
+      userName,
+      email,
+    ];
+  }
+}
