aac731f3fc81cd515c35fdab60764ad1d7b36b5e
arl <arl@anadeainc.com>
2024-04-13 13:01:35 +0300

    add usecase and failure

---
+++ lib/core/failure.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final Object? message;
+
+  const Failure([this.message]);
+
+  @override
+  List<Object?> get props => [message];
+}
+
+class NetworkFailure extends Failure {}
+
+class InternalAppFailure extends Failure {
+  final StackTrace? stackTrace;
+
+  const InternalAppFailure(super.message, [this.stackTrace]);
+
+  @override
+  List<Object?> get props => [message, stackTrace];
+}
